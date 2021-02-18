5126b8b45be05612c16b239937eef906e92a32e6
Lauro Ribeiro <lauroribeirola@hotmail.com>
2021-02-18 00:52:11 -0300

    Implement limited funcionality

--- script.js
+++ script.js
@@ -1,18 +1,65 @@
 const numButtons = document.querySelectorAll(".numbers");
+const opButtons = document.querySelectorAll(".operators");
 const display = document.querySelector(".display");
 const clearButton = document.querySelector("#clear");
-clearButton.addEventListener("click", () => display.textContent = "0");
-numButtons.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-        if(display.textContent == 0) display.textContent = "";
-        display.textContent += e.target.textContent;
-    });
+delButton = document.querySelector("#del");
+let operation = null;
+let operationSelected = false;
+let firstValue = null;
+let secondValue = null;
+
+let callbackFunctions = {
+    "add": add,
+    "subtract": subtract,
+    "multiply": multiply,
+    "divide": divide,
+};
+
+delButton.addEventListener("click", () => {
+    if(display.textContent.length == 0) return;
+    display.textContent = display.textContent.split("").slice(0,-1).join("");
 });
+
+opButtons.forEach((btn) => btn.addEventListener("click", (e) => {
+    let result = null;
+    if(!firstValue) firstValue = parseInt(display.textContent);
+    if (!operation) operation = e.target.id;
+    console.log(operation);
+    operationSelected = true;
+    if(e.target.id == "equal"){
+        secondValue = parseInt(display.textContent);
+        console.log(firstValue);
+        console.log(secondValue);
+        result = operate(callbackFunctions[operation], firstValue, secondValue);
+        console.log(result);
+        clear();
+    } 
+    if(operation == "invertSignal") result = invertSignal(parseInt(display.textContent));
+    
+    if(result) display.textContent = result;
+}));
+
+clearButton.addEventListener("click", clear);
+
+numButtons.forEach((btn) => btn.addEventListener("click", (e) => {
+    if(display.textContent == 0 || operationSelected){
+        display.textContent = "";
+        operationSelected = false;
+    }
+    display.textContent += e.target.textContent;
+}));
+
+function clear(){
+    display.textContent = "0";
+    operation = null;
+    operationSelected = false;
+    firstValue = null;
+    secondValue = null;
+}
 
 function add(a, b){
     return a + b;
 }
-
 function subtract(a, b){
     return a - b;
 }
@@ -23,7 +70,9 @@ function divide(a, b){
     if(b == 0) return "Math ERROR";
     return a / b;
 }
-
-function operate(operation, a, b){
-    return operation(a, b);
+function invertSignal(a){
+    return -a;
+}
+function operate(callback, a, b){
+    return callback(a, b);
 }
